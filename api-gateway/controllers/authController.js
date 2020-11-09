8e96f433bb4afa57645c992cc49079f7c56bc305
Sarah Massie <smassie@my365.bellevue.edu>
2020-11-08 22:19:25 -0800

    master

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -2,24 +2,48 @@
 ============================================
 ; Title:  authController.js
 ; Author: Sarah Massie
-; Date:   2 November 2020
+; Modified Date:   8 November 2020
 ; Description: creating the user registration and token for REST
 ;===========================================
 */
 var User = require('../models/users');
-
-// Register a new user on POST
-exports.user_register = function(req, res) {
-    res.send('NOT IMPLEMENTED: User registration POST');
-};
-
-// Verify token on GET
-exports.user_token = function(req, res) {
-    res.send('NOT IMPLEMENTED: User token lookup GET');
-};
 
 // user_token functions/require statements
 var User = require('../models/users');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
+const { restart } = require('nodemon');
+
+// Register a new user on POST
+exports.user_register = function(req, res) {
+    var secretPassword = bcrypt.hashSync(req.body.password, 8);
+    var newUser = new User ({
+        username: req.body.username,
+        password: secretPassword,
+        email: req.body.email
+    })
+
+    User.add(newUser, (err, user) => {
+        if (err)
+            return res.status(500).send('There was a problem registering the user.');
+        var token = jwt.sign({ id: user._id}, config.web.secret, {
+            expiresIn: 86400 // 24 hours
+        });
+        res.status(200).send({auth: true, token: token});
+    })
+};
+
+// Verify token on GET
+exports.user_token = function(req, res) {
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({auth: false, message: 'No token provided'});
+    jwt.verify(token, config.web.secret, function(err,decoded) {
+        if (err) return res.status(500).send({auth: false, message: 'Failed to authenticate token'})
+        User.getById(decoded.id, function(err, user) {
+            if (err) return res.status(500).send('There was a problem finding the user.');
+            if (!user) return res.status(404).send('No user found');
+            res.status(200).send(user);
+        })
+    })    
+};
