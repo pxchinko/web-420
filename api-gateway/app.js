81abb0846ef91a5c2737f0e81609d2e2ec9c9022
Sarah Massie <smassie@my365.bellevue.edu>
2020-12-05 22:50:05 -0800

    master

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -38,18 +38,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', apiCatalog);
 
 app.use('/', indexRouter);
+app.use('/api', apiCatalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -8,20 +8,21 @@
 */
 // API Routes
 var express = require('express');
-const checkToken = require('../check-token');
+var checkToken = require('../check-token');
 var router = express.Router();
+
 var auth_controller = require('../controllers/authController');
 
 // POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
 
-// POST request for logging in a user
-router.post('/auth/login', auth_controller.user_login);
-
 // GET request for verifying user tokens
 router.get('/auth/token', checkToken, auth_controller.user_token);
 
-// GET request for logging out
+// POST request for signing users in
+router.post('/auth/login', auth_controller.user_login);
+
+// GET request for logging users out
 router.get('/auth/logout', auth_controller.user_logout);
 
-module.exports = router;
+module.exports = router; 
